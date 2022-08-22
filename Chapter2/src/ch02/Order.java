86b06e404373695f646629c8c9dcd919c578953d
kimccchong <49267413+KimChaeHong@users.noreply.github.com>
2022-08-22 13:46:49 +0900

    Delete Chapter2 directory

--- Chapter2/src/ch02/Order.java
+++
@@ -1,9 +0,0 @@
-package ch02;
-
-public class Order {
-	int orderID;
-	String buyerId;
-	String sellerID;
-	int productID;
-	String orderDate;
-}
--- Chapter2/src/ch09/Subject.java
+++
@@ -1,7 +0,0 @@
-package ch09;
-
-public class Subject {
-	String subjectname;
-	int score;
-	int subjectId;
-}
